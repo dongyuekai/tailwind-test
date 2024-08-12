f13dadd30a7a022cd30cfd69dcd5472302ccb809
dongyuekai <457545648@qq.com>
2024-08-12 14:10:24 +0800

    feat: tailwindcss的signal机制

--- src/App.js
+++ src/App.js
@@ -13,8 +13,20 @@ function App() {
       <div className='btn-primary mt-7'>
         3333
       </div>
+      <button class='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded peer'>
+        鼠标悬停到此处
+      </button>
+      <div class='peer-hover:signal'>
+        <div class='signal:bg-green-800 bg-red-800 p-1 text-white'>
+          鼠标悬停到按钮我会变色
+        </div>
+      </div>
+      <div class='has-[:is(input:checked,div:hover)]:signal'>
+        <input type='checkbox' />
+        <div class='bg-red-800 p-1 text-white signal:bg-green-800'>鼠标悬停或勾选</div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
